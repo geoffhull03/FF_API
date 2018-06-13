cde6bf12b7be6345d9a54ec115bcf4a40a73d278
Geoff Hull <geoffhull03@gmail.com>
2018-06-13 15:19:22 -0700

    Adds rspec

--- Gemfile
+++ Gemfile
@@ -49,3 +49,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :test do
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'ffaker'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+FfApi::Application.routes.draw do
+  namespace :api, defaults: { format: :json },
+                            constraints: { subdomain: 'api' }, path: '/'  do
+            scope module: :v1 do
+        end
+    end
 end
