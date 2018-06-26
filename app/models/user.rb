78c98932fe3c632a33bf53b3e40ccab7994560a8
Geoff Hull <geoffhull03@gmail.com>
2018-06-26 14:49:29 -0700

    Initial User specs pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def change
+    create_table :contents do |t|
+      t.ingeger "id"
+      t.string "name"
+      t.string "email"
+
+      t.timestamps
+    end
+  end
 end
