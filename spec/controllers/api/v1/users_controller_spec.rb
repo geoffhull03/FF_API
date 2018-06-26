37793a1352970828925194b3ab6aad4ee801a442
Geoff Hull <geoffhull03@gmail.com>
2018-06-26 14:11:29 -0700

    Backtracked to work on users

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Api::V1::UsersController do
+  before(:each) { request.headers['Accept'] = "application/ffapi.v1" }
+
+  describe "GET #SHOW" do
+    before (:each) do
+      @user = FactoryBot.create :user
+      get :show, params: {id: @user_id, format: :json}
+    end
+
+    it "returns the information about a reporter on a hash" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql @user.email
+    end
+
+    it { should respond_with 200}
+  end
+end
